bdd19c8f7992d1e31dd050ed7fa57528148d22e7
sxclij <sxclij@gmail.com>
2023-12-27 15:46:36 +0900

    add macro

---
+++ macro/main.py
@@ -0,0 +1,43 @@
+import csv
+import pprint
+import pyautogui
+
+path = "C:/u/vscode/repo/sxclij_mc/cb/tick/item/cb.csv"
+
+with open(path) as f:
+    reader = csv.reader(f, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+
+    i = 0
+    for row in reader:
+        
+        pyautogui.press("t")
+        x = ""
+        match row[0]:
+            case "repeat":
+                x = "/setblock ~ ~ ~1 repeating_command_block"
+            case "chain":
+                x = "/setblock ~ ~ ~1 chain_command_block"
+            case "air":
+                x = "/setblock ~ ~ ~1 air"
+        match row[1]:
+            case "conditional":
+                x += "[conditional=true,facing=up]"
+            case "unconditional":
+                x += "[conditional=false,facing=up]"
+        pyautogui.write(x)
+        pyautogui.press("enter")
+
+        if(row[2] == "always"):
+            pyautogui.press("t")
+            pyautogui.write("/data modify block ~ ~ ~1 auto set value 1b")
+            pyautogui.press("enter")
+    
+        pyautogui.press("t")
+        pyautogui.write("/data modify block ~ ~ ~1 Command set value \"" + row[3] + "\"")
+        pyautogui.press("enter")
+        
+        pyautogui.press("t")
+        pyautogui.write("/tp @s ~ ~1 ~")
+        pyautogui.press("enter")
+
+        i+=1
